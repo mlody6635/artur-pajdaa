d60efb5f6c5d1fbe2f54f785a0654f7731387db9
mlody6635 <arturpajdaa@gmail.com>
2020-04-16 23:29:48 +0100

    first commit

---
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.kodillacourse;
+
+class Calculator {
+    double a;
+    double b;
+    public double adding(double a , double b){
+        return a+b;
+    }
+    public double subtracting(double a, double b){
+        return a-b;
+    }
+}
